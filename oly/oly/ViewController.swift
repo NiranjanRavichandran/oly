cc850bb6b78a75aaa8616fb90b4aceac1f11df2b
Niranjan Ravichandran <niranjanrav@gmail.com>
2018-05-31 12:37:08 -0400

    Initial commit

---
+++ oly/oly/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  oly
+//
+//  Created by Niranjan Ravichandran on 5/31/18.
+//  Copyright © 2018 Aviato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ oly/watch Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  watch Extension
+//
+//  Created by Niranjan Ravichandran on 5/31/18.
+//  Copyright © 2018 Aviato. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
